867f52b331ecf6a1820d1f37f2e7bc36fb6f467b
garlic0000 <garlic0000@163.com>
2024-03-22 19:50:31 +0800

    更新

--- PaperNameToPDFName.py
+++ PaperNameToPDFName.py
@@ -1,6 +1,6 @@
 def main():
     #
-    str1 = "Facial Micro-expressions Analysis: Its Databases, Feature Extraction, and Classification Methods"
+    str1 = "Emotion-specific AUs for micro-expression recognition"
     str2 = str1.replace(' ', '_')
     print(str2)
 
--- test2.py
+++ test2.py
@@ -9,7 +9,7 @@ def getresponse(api_key, api_secret, return_landmark, img_path):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br, zsdch",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6", 
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
         "Connection": "keep-alive"
     }
     data = {
