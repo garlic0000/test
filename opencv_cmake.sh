270a70c2737a6075c372d980bda75b70888a4a7d
garlic0000 <garlic0000@163.com>
2024-10-19 19:53:54 +0800

    Update opencv_cmake.sh

--- opencv_cmake.sh
+++ opencv_cmake.sh
@@ -16,10 +16,8 @@ cmake \
     -DPYTHON3_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
     -DPYTHON3_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
     -DPYTHON3_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
-    -DOpenBLAS_INCLUDE_DIR=/usr/include/x86_64-linux-gnu/openblas-pthread \
-    -DOpenBLAS_LIB=/usr/lib/x86_64-linux-gnu/libopenblas.so \
+    -DOpenBLAS_INCLUDE_DIR=/usr/include/x86_64-linux-gnu \
     -DLAPACKE_INCLUDE_DIR=/usr/include \
-    -DLAPACKE_LIBRARIES=/usr/lib/x86_64-linux-gnu/liblapacke.so \
     -DBUILD_SHARED_LIBS=OFF \
     -DWITH_SIMD=ON \
     -S $OPENCV_SOURCE_DIR \
