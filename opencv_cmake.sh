474e5030c1ee4aad39b5960a28147871bf2fd7d2
garlic0000 <garlic0000@163.com>
2024-10-19 15:00:25 +0800

    Update opencv_cmake.sh

--- opencv_cmake.sh
+++ opencv_cmake.sh
@@ -13,9 +13,9 @@ cmake \
     -DCUDA_ARCH_BIN="5.0;5.2;6.0;6.1;7.0;7.5" \
     -DCUDA_ARCH_PTX="6.0;6.1" \
     -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
-    -DPYTHON_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
-    -DPYTHON_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
-    -DPYTHON_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
+    -DPYTHON3_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
+    -DPYTHON3_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
+    -DPYTHON3_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
     -S $OPENCV_SOURCE_DIR \
     -B $OPENCV_BUILD_DIR  # 指定构建目录
 
