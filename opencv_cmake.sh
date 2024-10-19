33b8405eb478039c5b32f57ed764eaa4ac082a45
garlic0000 <garlic0000@163.com>
2024-10-19 13:34:59 +0800

    Update opencv_cmake.sh

--- opencv_cmake.sh
+++ opencv_cmake.sh
@@ -1,11 +1,11 @@
-cmake -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
-      -D WITH_CUDA=ON \
-      -D CUDA_ARCH_BIN="5.0;5.2;6.0;6.1;7.0;7.5" \
-      -D CUDA_ARCH_PTX="6.0;6.1" \
-      -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
-      -D PYTHON_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
-      -D PYTHON_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
-      -D PYTHON_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/usr/local \
+      -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+      -DWITH_CUDA=ON \
+      -DCUDA_ARCH_BIN="5.0;5.2;6.0;6.1;7.0;7.5" \
+      -DCUDA_ARCH_PTX="6.0;6.1" \
+      -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
+      -DPYTHON_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
+      -DPYTHON_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
+      -DPYTHON_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
       ..
