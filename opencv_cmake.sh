38fe00331fd7ab7466db4194e51c5b6383e57754
garlic0000 <garlic0000@163.com>
2024-10-20 14:34:31 +0800

    Update opencv_cmake.sh

--- opencv_cmake.sh
+++ opencv_cmake.sh
@@ -11,17 +11,12 @@ cmake \
     -DCUDA_ARCH_BIN="5.0;5.2;6.0;6.1;7.0;7.5" \
     -DCUDA_ARCH_PTX="6.0;6.1" \
     -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
-    -DCUDA_CUDA_LIBRARY=/usr/local/cuda/lib64/libcudart.so \
-    -DCUDA_LIBRARY=/usr/local/cuda/lib64/libcudart.so \
     -DPYTHON3_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
     -DPYTHON3_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
     -DPYTHON3_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
     -DOpenBLAS_INCLUDE_DIR=/usr/include/x86_64-linux-gnu/openblas-pthread \
     -DOpenBLAS_LIB=/usr/lib/x86_64-linux-gnu/libopenblas.so \
     -DBUILD_SHARED_LIBS=OFF \
-    -DWITH_SIMD=ON \
-    -DOPENSSL_ROOT_DIR=/usr \
-    -DOPENSSL_LIBRARIES=/usr/lib/x86_64-linux-gnu \
     -S $OPENCV_SOURCE_DIR \
     -B $OPENCV_BUILD_DIR
 
