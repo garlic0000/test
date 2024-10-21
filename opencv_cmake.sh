9cf10e293530ad5c45d1a6a1b4797c8e3a31e5b6
garlic0000 <garlic0000@163.com>
2024-10-21 08:12:09 +0800

    Update opencv_cmake.sh

--- opencv_cmake.sh
+++ opencv_cmake.sh
@@ -11,7 +11,6 @@ cmake \
     -DCUDA_ARCH_BIN="5.0;5.2;6.0;6.1;7.0;7.5" \
     -DCUDA_ARCH_PTX="6.0;6.1" \
     -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
-    -DBUILD_opencv_python3=ON \
     -DPYTHON3_EXECUTABLE=/opt/conda/envs/newCondaEnvironment/bin/python \
     -DPYTHON3_INCLUDE_DIR=/opt/conda/envs/newCondaEnvironment/include/python3.10 \
     -DPYTHON3_LIBRARY=/opt/conda/envs/newCondaEnvironment/lib/libpython3.10.so \
